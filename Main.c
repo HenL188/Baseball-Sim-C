575739fecd26c7ed9f58fb8a0a0460b49c914916
HenL188 <153272326+HenL188@users.noreply.github.com>
2024-04-28 19:26:48 -0600

    not much better

--- Main.c
+++ Main.c
@@ -41,19 +41,12 @@ int main() {
 	printf("Score:%d", score);
 }
 
-float Rate() {
-	float array[] = { 1.0, 1.25, 1.5, 1.75, 2.0, 2.25, 2.5, 2.75, 3.0, 3.25, 3.5, 3.75, 4.0 };
-	srand(time(0));
-	int index = rand() % 13;
-	float rate = array[index];
-	return rate;
-}
-
 bool Walk(float f) {
 	bool walk = false;
-	float walkRate = Rate();
+	srand(time(0));
+	int walkRate = rand() % 100;
 
-	if (walkRate < f) {
+	if (walkRate <= f) {
 		walk = true;
 		return walk;
 	}
@@ -63,9 +56,10 @@ bool Walk(float f) {
 
 bool Hit(float f) {
 	bool hit = false;
-	float hitRate = Rate();
+	srand(time(0));
+	int hitRate = rand() % 100;
 
-	if (hitRate < f) {
+	if (hitRate <= f) {
 		hit = true;
 		return hit;
 	}
@@ -74,23 +68,24 @@ bool Hit(float f) {
 }
 
 bool Double(float f) {
-	float doubleRate = Rate();
 	bool doubleHit = false;
+	srand(time(0));
+	int doubleRate = rand() % 100;
 
-	if (doubleRate < f && firstBase == true) {
+	if (doubleRate <= f && firstBase == true) {
 		thirdBase = true;
 		secoundBase = true;
 		doubleHit = true;
 		return doubleHit;
 	}
-	else if (doubleRate < f && firstBase == true && secoundBase == true) {
+	else if (doubleRate <= f && firstBase == true && secoundBase == true) {
 		thirdBase = true;
 		secoundBase = true;
 		score += 1;
 		doubleHit = true;
 		return doubleHit;
 	}
-	else if (doubleRate < f && firstBase == true && secoundBase == true && thirdBase == true) {
+	else if (doubleRate <= f && firstBase == true && secoundBase == true && thirdBase == true) {
 		thirdBase = true;
 		secoundBase = true;
 		firstBase = false;
@@ -98,14 +93,14 @@ bool Double(float f) {
 		doubleHit = true;
 		return doubleHit;
 	}
-	else if (doubleRate < f && firstBase == true && thirdBase == true) {
+	else if (doubleRate <= f && firstBase == true && thirdBase == true) {
 		thirdBase = true;
 		secoundBase = true;
 		score += 1;
 		doubleHit = true;
 		return doubleHit;
 	}
-	else if (doubleRate < f && secoundBase == true && thirdBase == true) {
+	else if (doubleRate <= f && secoundBase == true && thirdBase == true) {
 		thirdBase = true;
 		secoundBase = true;
 		firstBase = false;
@@ -113,7 +108,7 @@ bool Double(float f) {
 		doubleHit = true;
 		return doubleHit;
 	}
-	else if (doubleRate < f) {
+	else if (doubleRate <= f) {
 		secoundBase = true;
 		doubleHit = true;
 		return doubleHit;
@@ -124,34 +119,35 @@ bool Double(float f) {
 }
 
 bool Triple(float f) {
-	float tripleRate = Rate();
 	bool triple = false;
+	srand(time(0));
+	int tripleRate = rand() % 100;
 
-	if (tripleRate < f && firstBase == true) {
+	if (tripleRate <= f && firstBase == true) {
 		thirdBase = true;
 		score += 1;
 		triple = true;
 		return triple;
 	}
-	else if (tripleRate < f && firstBase == true && secoundBase == true) {
+	else if (tripleRate <= f && firstBase == true && secoundBase == true) {
 		thirdBase = true;
 		score += 2;
 		triple = true;
 		return triple;
 	}
-	else if (tripleRate < f && firstBase == true && secoundBase == true && thirdBase == true) {
+	else if (tripleRate <= f && firstBase == true && secoundBase == true && thirdBase == true) {
 		thirdBase = true;
 		score += 3;
 		triple = true;
 		return triple;
 	}
-	else if (tripleRate < f && firstBase == true && thirdBase == true) {
+	else if (tripleRate <= f && firstBase == true && thirdBase == true) {
 		thirdBase = true;
 		score += 2;
 		triple = true;
 		return triple;
 	}
-	else if (tripleRate < f && secoundBase == true && thirdBase == true) {
+	else if (tripleRate <= f && secoundBase == true && thirdBase == true) {
 		thirdBase = true;
 		score += 2;
 		triple = true;
@@ -168,45 +164,46 @@ bool Triple(float f) {
 }
 
 bool HomeRun(float f) {
-	float homeRate = Rate();
 	bool homeRun = false;
+	srand(time(0));
+	int homeRate = rand() % 100;
 
-	if (homeRate < f && firstBase == true) {
+	if (homeRate <= f && firstBase == true) {
 		score += 2;
 		homeRun = true;
 		return homeRun;
 	}
-	else if (homeRate < f && firstBase == true && secoundBase == true) {
+	else if (homeRate <= f && firstBase == true && secoundBase == true) {
 		score += 3;
 		homeRun = true;
 		return homeRun;
 	}
-	else if (homeRate < f && firstBase == true && secoundBase == true && thirdBase == true) {
+	else if (homeRate <= f && firstBase == true && secoundBase == true && thirdBase == true) {
 		score += 4;
 		homeRun = true;
 		return homeRun;
 	}
-	else if (homeRate < f && firstBase == true && thirdBase == true) {
+	else if (homeRate <= f && firstBase == true && thirdBase == true) {
 		score += 3;
 		homeRun = true;
 		return homeRun;
 	}
-	else if (homeRate < f && secoundBase == true && thirdBase == true) {
+	else if (homeRate <= f && secoundBase == true && thirdBase == true) {
 		score += 3;
 		homeRun = true;
 		return homeRun;
 	}
-	else if (homeRate < f && secoundBase == true) {
+	else if (homeRate <= f && secoundBase == true) {
 		score += 2;
 		homeRun = true;
 		return homeRun;
 	}
-	else if (homeRate < f && thirdBase == true) {
+	else if (homeRate <= f && thirdBase == true) {
 		score += 2;
 		homeRun = true;
 		return homeRun;
 	}
-	else if (homeRate < f) {
+	else if (homeRate <= f) {
 		score += 1;
 		homeRun = true;
 		return homeRun;
@@ -217,9 +214,10 @@ bool HomeRun(float f) {
 }
 
 bool StrikeOut(float f) {
-	float strikeOutRate = Rate();
 	bool strikeout = false;
-	if (strikeOutRate < f) {
+	srand(time(0));
+	int strikeOutRate = rand() % 100;
+	if (strikeOutRate <= f) {
 		teamOuts -= 1;
 		strikeout = true;
 		return strikeout;
@@ -228,12 +226,13 @@ bool StrikeOut(float f) {
 }
 
 bool GroundOut(float f) {
-	float groundOutRate = Rate();
 	bool groundOut = false;
+	srand(time(0));
+	int groundOutRate = rand() % 100;
 	srand(time(0));
 	int ground = rand() % 10;
 
-	if (groundOutRate < f) {
+	if (groundOutRate <= f) {
 
 		if (firstBase == true && ground <= 1) {
 			firstBase = false;
@@ -247,23 +246,24 @@ bool GroundOut(float f) {
 }
 
 bool FlyOut(float f) {
-	float flyout = Rate();
 	bool flyOut = false;
+	srand(time(0));
+	int flyout = rand() % 100;
 
-	if (flyout < f && firstBase == true) {
+	if (flyout <= f && firstBase == true) {
 		teamOuts -= 1;
 		secoundBase = true;
 		flyOut = true;
 		return flyOut;
 	}
-	else if (flyout < f && firstBase == true && secoundBase == true) {
+	else if (flyout <= f && firstBase == true && secoundBase == true) {
 		teamOuts -= 1;
 		secoundBase = true;
 		thirdBase = true;
 		flyOut = true;
 		return flyOut;
 	}
-	else if (flyout < f && firstBase == true && secoundBase == true && thirdBase == true) {
+	else if (flyout <= f && firstBase == true && secoundBase == true && thirdBase == true) {
 		teamOuts -= 1;
 		secoundBase = true;
 		thirdBase = true;
@@ -271,33 +271,33 @@ bool FlyOut(float f) {
 		flyOut = true;
 		return flyOut;
 	}
-	else if (flyout < f && firstBase == true && thirdBase == true) {
+	else if (flyout <= f && firstBase == true && thirdBase == true) {
 		teamOuts -= 1;
 		secoundBase = true;
 		score += 1;
 		flyOut = true;
 		return flyOut;
 	}
-	else if (flyout < f && secoundBase == true) {
+	else if (flyout <= f && secoundBase == true) {
 		teamOuts -= 1;
 		thirdBase = true;
 		flyOut = true;
 		return flyOut;
 	}
-	else if (flyout < f && thirdBase == true) {
+	else if (flyout <= f && thirdBase == true) {
 		teamOuts -= 1;
 		score += 1;
 		flyOut = true;
 		return flyOut;
 	}
-	else if (flyout < f && thirdBase == true && secoundBase == true) {
+	else if (flyout <= f && thirdBase == true && secoundBase == true) {
 		teamOuts -= 1;
 		thirdBase = true;
 		score += 1;
 		flyOut = true;
 		return flyOut;
 	}
-	else if (flyout < f) {
+	else if (flyout <= f) {
 		teamOuts -= 1;
 		flyOut = true;
 		return flyOut;
@@ -306,10 +306,11 @@ bool FlyOut(float f) {
 }
 
 bool PopUp(float f) {
-	float popOutRate = Rate();
 	bool popOut = false;
+	srand(time(0));
+	int popOutRate = rand() % 100;
 
-	if (popOutRate < f) {
+	if (popOutRate <= f) {
 		teamOuts -= 1;
 		popOut = true;
 		return popOut;
@@ -317,66 +318,84 @@ bool PopUp(float f) {
 	return popOut;
 }
 
-bool LineDrive(float f) {
-	float  lineDriveRate = Rate();
-	bool lineDrive = false;
-
-	if (lineDriveRate < f) {
-		teamOuts -= 1;
-		lineDrive = true;
-		return lineDrive;
-	}
-	return lineDrive;
-}
 
 void Hitter() {
-	bool walk = Walk(1);
-			bool hit = Hit(2.66);
+	bool walk = Walk(8);
+	bool hit = Hit(26.6);
 
-			if (walk == true) {
+	if (walk == true) {
+		firstBase = true;
+		printf("Walk!\n");
+	}
+	if (hit == true) {
+		bool doubleHit = Double(1.5);
+		bool triple = Triple(1.5);
+		bool homeRun = HomeRun(1.5);
+		if (doubleHit == true) {
+			printf("Double!\n");
+		}
+		else if (triple == true) {
+			printf("Triple!\n");
+		}
+		else if (homeRun == true) {
+			printf("Home Run!\n");
+		}
+		else
+		{
+			printf("Hit!\n");
+			if (firstBase == true) {
+				secoundBase = true;
 				firstBase = true;
-				printf("Walk!\n");
 			}
-			else if (hit == true) {
-				bool doubleHit = Double(1.5);
-				bool triple = Triple(1.5);
-				bool homeRun = HomeRun(1.5);
-				if (doubleHit == true) {
-					printf("Double!\n");
-				}
-				else if (triple == true) {
-					printf("Triple!\n");
-				}
-				else if (homeRun == true) {
-					printf("Home Run!\n");
-				}
-				else
-				{
-					firstBase = true;
-					printf("Hit!\n");
-				}
+			else if (firstBase == true && secoundBase == true) {
+				thirdBase = true;
+				firstBase = true;
 			}
-			else if (hit == false) {
-				bool strike = StrikeOut(2.18);
-				if (strike == true) {
-					printf("Strike :(\n");
-				}
-				bool ground = GroundOut(3.28);
-				if (strike == false && ground == true) {
-					printf("Ground Out :(\n");
-				}
-				bool fly = FlyOut(2.13);
-				if (fly == true && strike == false && ground == false) {
-					printf("Fly Out :(\n");
-				}
-				bool line = LineDrive(3.28);
-				if (line == true && strike == false && fly == false && ground == false) {
-					printf("Line Drive :(\n");
-				}
-				bool pop = PopUp(0.33);
-				if (pop == true && strike == false && fly == false && line == false && ground == false) {
-					printf("Popup :(\n");
-				}
+			else if (firstBase == true && secoundBase == true && thirdBase == true) {
+				firstBase = true;
+				secoundBase = true;
+				thirdBase = true;
+				score += 1;
 			}
-}
+			else if (firstBase == true && thirdBase == true) {
+				firstBase = true;
+				secoundBase = true;
+				score += 1;
+			}
+			else if (secoundBase == true && thirdBase == true) {
+				thirdBase = true;
+				score += 1;
+			}
+		}
+		
+		}
+	else {
+		bool strike = StrikeOut(21.8);
+		if (strike == true) {
+			printf("Strike :(\n");
+		}
+		else if (strike == false) {
+			bool ground = GroundOut(32.8);
+			if (ground == true) {
+				printf("Ground Out :(\n");
+			}
+		}
+		else if (strike == false) {
+			bool fly = FlyOut(21.3);
+			if (fly == true) {
+				printf("Fly Out :(\n");
+			}
+		}
+		else if (strike == false) {
+			bool pop = PopUp(3.3);
+			if (pop == true) {
+				printf("Popup :(\n");
+			}
 
+		}
+		else {
+			printf("Line Drive :(\n");
+			teamOuts -= 1;
+		}
+	}
+}
--- Main.h
+++ Main.h
@@ -2,7 +2,6 @@
 #include<stdio.h>
 #include <stdbool.h>
 
-float Rate();
 bool Walk(float f);
 bool Hit(float f);
 bool Double(float f);
@@ -12,7 +11,6 @@ bool StrikeOut(float f);
 bool GroundOut(float f);
 bool FlyOut(float f);
 bool PopUp(float f);
-bool LineDrive(float f);
 
 void Hitter();
 
